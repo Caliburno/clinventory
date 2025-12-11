121d7c74cc3d7d8dcd6fcddf19648ca0ad79fae0
Caliburno <dario.buffa@gmail.com>
2025-12-11 18:42:57 -0300

    Change db path

--- src/config.py
+++ src/config.py
@@ -1,1 +1,11 @@
-DB_PATH = "clinventory.db"
+import os
+import sys
+
+# Detects if the application is running in a frozen state (e.g., packaged with PyInstaller)
+if getattr(sys, "frozen", False):
+    BASE_DIR = os.path.dirname(sys.executable) # Path to the executable
+else:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # Path to the current script
+
+# Define the path to the SQLite database file
+DB_PATH = os.path.join(BASE_DIR, "clinventory.db")
